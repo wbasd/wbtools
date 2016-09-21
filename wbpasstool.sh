23581eb15969b4a60a0aa81fc7f93fc373bd3a77
Aaron Prisk <aaronprisk@gmail.com>
2016-09-21 11:16:07 -0400

    Added user confirmation loop

--- wbpasstool.sh
+++ wbpasstool.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
-echo "***************************************************"
-echo "*************** WEST BRANCH PASSWORD **************"
-echo "***************   UPDATE TOOL V1.0   **************"
-echo "***************************************************"
-echo
-echo "Please follow the prompts to update your WB Account"
-echo "----------------------------------------------------"
-echo
-echo "Please enter your username (EX: 101830 OR jsmith)"
-read user
-smbpasswd -U $user -r [SERVER-ADDRESS]
-read -s -n 1 -p "PRESS ANY KEY TO CLOSE"
-exit
+while true;
+do
+	clear
+	echo "***************************************************"
+	echo "*************** WEST BRANCH PASSWORD **************"
+	echo "***************   UPDATE TOOL V1.0   **************"
+	echo "***************************************************"
+	echo
+	echo "Please follow the prompts to update your WB Account"
+	echo "---------------------------------------------------"
+	echo
+	echo "Please enter your username (EX: 120123 OR jsmith)"
+	read user
+	echo "*************************************************"
+	echo "$user, you will now be prompted to change your WB"
+	echo "password. A good password is 8+ characters with"
+	echo "a mix of numbers and special characters."
+	echo "*************************************************"
+
+	smbpasswd -U $user -r wolverine.westbranch.org
+
+	echo "*************************************************"
+	read -r -p "Was password change successful? [Y/n] " input
+
+	case $input in
+	    [yY][eE][sS]|[yY])
+			clear
+			exit
+			;;
+	    [nN][oO]|[nN])
+			echo "..."
+			;;
+	    *)
+		echo "INVALID INPUT (Enter Y or N)"
+		;;
+	esac
+done
